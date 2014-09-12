e986b69727fd2bad6624b4845c17cebbfc7377a5
amyvs <amydecat@yahoo.com>
2014-09-12 11:07:27 -0700

    add strong params to trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,28 @@ class TripsController < ApplicationController
   def index
     @trips = Trips.all
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+    if @trip.valid?
+      flash[:notice] = "Your trip has been documented!"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Something went wrong. Please try again."
+      render "new"
+    end
+  end
+
+  def show
+    @trip = Trip.find(trip_params)
+  end
+
+private
+  def trip_params
+    params.require(:trip).permit(:description).merge(:user_id => current_user.id)
+  end
 end
