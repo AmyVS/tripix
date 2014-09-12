d89a09ecb4d5d7bb4f666b9dc60ea50f0930b8ad
amyvs <amydecat@yahoo.com>
2014-09-12 10:36:27 -0700

    validate presence of description for trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ActiveRecord::Base
   validates :user_id, presence: true
+  validates :description, presence: true
 
 end
--- spec/trip_spec.rb
+++ spec/trip_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Trip do
   it { should validate_presence_of :user_id }
+  it { should validate_presence_of :description }
 end
