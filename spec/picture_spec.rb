88bf212a3c5afdd204c455c36130d0e07e35e76e
amyvs <amydecat@yahoo.com>
2014-09-12 11:54:00 -0700

    add association picture belongs to user and trip

--- spec/picture_spec.rb
+++ spec/picture_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Picture do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :trip_id }
-
+  it { should belong_to :user }
+  it { should belong_to :trip }
 end
