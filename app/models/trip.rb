d8e03bed1afbdfd9a0428d6ef6ad522abf401b26
amyvs <amydecat@yahoo.com>
2014-09-12 10:34:49 -0700

    validate presence of user_id for trip

---
+++ app/models/trip.rb
@@ -0,0 +1,4 @@
+class Trip < ActiveRecord::Base
+  validates :user_id, presence: true
+
+end
---
+++ spec/trip_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe Trip do
+  it { should validate_presence_of :user_id }
+end
