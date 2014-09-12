d8918e556f9949e83ac5683907581cd06ad0ef38
amyvs <amydecat@yahoo.com>
2014-09-12 11:55:21 -0700

    add association trip has many pictures

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,5 @@ class Trip < ActiveRecord::Base
   validates :user_id, presence: true
   validates :name, presence: true
   belongs_to :user
+  has_many :pictures
 end
--- spec/trip_spec.rb
+++ spec/trip_spec.rb
@@ -4,4 +4,5 @@ describe Trip do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :name }
   it { should belong_to :user }
+  it { should have_many :pictures }
 end
