2c5b23599273ff81c6af19facdc95581758f8356
Amy Vaillancourt-Sals <amydecat@yahoo.com>
2014-09-15 15:45:23 -0700

    make sure a trip name is not blank before editing it

--- spec/features/add_trip_spec.rb
+++ spec/features/add_trip_spec.rb
@@ -37,4 +37,17 @@ describe "adding trips" do
     click_button "Update Trip"
     expect(page).to have_content "Your trip has been edited."
   end
+
+  it "makes sure that a trip name is not blank before editing it" do
+    visit '/sessions/new'
+    user = User.create(:username => 'myname', :email => 'myname@email.com', :password => 'mypassword')
+    fill_in :email, :with => 'myname@email.com'
+    fill_in :password, :with => 'mypassword'
+    click_button "Enter"
+    trip = Trip.create(:name => 'my adventure', :user_id => user.id)
+    click_link("#{user.username}")
+    click_link('edit')
+    click_button "Update Trip"
+    expect(page).to have_content "Your trip has been edited."
+  end
 end
