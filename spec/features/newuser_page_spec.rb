6951c079cb3e5fbd08b0193657cc49a0ef4dd25e
Amy Vaillancourt-Sals <amydecat@yahoo.com>
2014-09-15 16:23:18 -0700

    ensure a user cannot be created if an email is missing

--- spec/features/newuser_page_spec.rb
+++ spec/features/newuser_page_spec.rb
@@ -10,4 +10,13 @@ describe "new user page" do
     click_button "Sign Up"
     expect(page).to have_content "Thanks for signing up!"
   end
+
+  it "does not create a new user when something is missing" do
+    visit 'signup'
+    fill_in :user_username, :with => 'me'
+    fill_in :user_password, :with => 'mypassword'
+    fill_in :user_password_confirmation, :with => 'mypassword'
+    click_button "Sign Up"
+    expect(page).to have_content "Please fix the following errors:"
+  end
 end
