157698583b1c9ef45e80f0b6baf5e86b7dc54b44
Amy Vaillancourt-Sals <amydecat@yahoo.com>
2014-09-15 09:53:11 -0700

    safe guard against users who enter a password that doesn't match their own

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -9,4 +9,13 @@ describe "the signin process" do
     click_button "Enter"
     page.should have_content "Logged in!"
   end
+
+  it "gives a user an error who enters a password that doesn't match their own" do
+    visit '/sessions/new'
+    user = User.create(:username => 'myname', :email => 'myname@email.com', :password => 'mypassword')
+    fill_in :email, :with => 'myname@email.com'
+    fill_in :password, :with => 'notmypassword'
+    click_button "Enter"
+    page.should have_content "Email or password is invalid"
+  end
 end
