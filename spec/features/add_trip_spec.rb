32c323288dbcb1e400053a68add80d849ea9cfbd
Amy Vaillancourt-Sals <amydecat@yahoo.com>
2014-09-15 14:59:07 -0700

    make sure a trip isn't added, if name has no content

--- spec/features/add_trip_spec.rb
+++ spec/features/add_trip_spec.rb
@@ -13,4 +13,15 @@ describe "adding trips" do
     click_button "Create Trip"
     expect(page).to have_content "Your trip has been documented!"
   end
+
+  it "makes sure that a trip name is added before adding it" do
+    visit '/sessions/new'
+    user = User.create(:username => 'myname', :email => 'myname@email.com', :password => 'mypassword')
+    fill_in :email, :with => 'myname@email.com'
+    fill_in :password, :with => 'mypassword'
+    click_button "Enter"
+    visit '/trips/new'
+    click_button "Create Trip"
+    expect(page).to have_content "Something went wrong. Please try again."
+  end
 end
